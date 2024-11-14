d0c6ac15f93a6a605ffe2e8b8e14746d887813bb
PedroMtz <thelop4lop@gmail.com>
2024-11-14 13:41:02 -0600

    Implementation of two new classes for deploy Information on world.

---
+++ ManofactureSimulator/ProductionScreen.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProductionScreen.h"
+
+// Sets default values
+AProductionScreen::AProductionScreen()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProductionScreen::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProductionScreen::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
